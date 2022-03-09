7f7b6a4523e84e145db34a16bb278a7e25e1b998
Lev Kurbanov <lev.k@smplt.net>
2022-03-09 17:23:17 +0300

    refactoring

--- src/MarketingBox.Postback.Service/Repositories/EventReferenceLoggerRepository.cs
+++ src/MarketingBox.Postback.Service/Repositories/EventReferenceLoggerRepository.cs
@@ -128,7 +128,7 @@ namespace MarketingBox.Postback.Service.Repositories
                 var limit = request.Take <= 0 ? 1000 : request.Take;
                 if (request.Asc)
                 {
-                    if (request.Cursor != null)
+                    if (request.Cursor.HasValue)
                     {
                         query = query.Where(x => x.Id > request.Cursor);
                     }
